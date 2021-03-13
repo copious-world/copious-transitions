55dbb3d1c9d80932f0498ccf62f07c4a6d821513
Richard Leddy <richardaleddy@gmail.com>
2021-03-12 17:05:18 -0800

    Removed directories that now have their own git repositories
    These are pulled into projects by npm

--- custom_storage/message-relay-services/index.js
+++
@@ -1,7 +0,0 @@
-const ClientMessageRelay = require('./lib/message_relay_client.js')
-const ServerMessageRelay = require('./lib/message_relay.js')
-const ServerMessageEndpoint = require('./lib/message_endpoint.js')
-
-module.exports.MessageRelayer = ClientMessageRelay
-module.exports.ServeMessageRelay = ServerMessageRelay
-module.exports.ServeMessageEndpoint = ServerMessageEndpoint
