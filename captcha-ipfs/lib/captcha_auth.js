77739cc90c329732a51cf5b72fdc68d5abf573ef
Richard Albert Leddy <richardaleddy@gmail.com>
2021-06-09 17:46:20 -0700

    Complete the crypto loop for captcha-ipfs
    Sign in now uses a single pathway depending on private keys stored within the user browser.
    Next: TEST

--- captcha-ipfs/lib/captcha_auth.js
+++ captcha-ipfs/lib/captcha_auth.js
@@ -4,6 +4,11 @@ const { GeneralAuth, SessionManager } = require.main.require('./lib/general_auth
 const cookieParser = require('cookie-parser');
 //const uuid = require('uuid/v4');
 
+const { Crypto } = require('node-webcrypto-ossl')
+const crypto = new Crypto()
+var g_crypto = crypto.subtle; //webcrypto.crypto.subtle
+
+
 var cnt = 0
 
 class CaptchaSessionManager extends SessionManager {
@@ -54,82 +59,163 @@ class CaptchaSessionManager extends SessionManager {
 
 
 
-    async ipfs_process_user(user_op,post_body,req,res,primary_key) {
+    async gen_cipher_key() {
+        //
+        try {
+            let aes_key = g_crypto.generateKey({
+                                                    name: "AES-CBC",
+                                                    length: 256
+                                                },
+                                                true,
+                                                ["encrypt", "decrypt"]
+                                            )	
 
-        let transtion_object = {
-            "token" : "nothing",
-            "secondary_action" : false,
-            "session_token" : undefined,
-            "type" : "user",
-            "user_op" : user_op
-        }
-
-        post_body = this.post_body_decode(post_body)
-
-        switch ( user_op ) {
-            case 'login' : {
-                if ( this.db ) {
-                    let user = await this.db.fetch_user(post_body)
-                    if ( user ) {
-                        if ( user.logged_in ) {
-                            //transtion_object.amelioritive_action = this.second_login_amelioration
-                            //break;
-                        }
-                        //
-                        if ( user.cid === post_body.cid ) {
-                            //
-                            transtion_object.secondary_action = true
-                            let key_key = this.key_for_user()
-                            transtion_object._t_u_key = key_key
-                            transtion_object.user_key = user[key_key]
-                            this.loginTransitionFields(transtion_object,post_body,user)
-                            let [wkey,aes_key] = await this.gen_wrapped_key(user.public_key)
-                            transtion_object.wrapped_key = wkey
-                            let challenge = gen_nonce()
-                            transtion_object.ctext = await this.cipher(challenge,aes_key)
-                            transtion_object.elements["clear"] = challenge
-                            transtion_object.elements["verify_key"] = user.signer_public_key
-                            //
-                        }
-                    } else {
-                        transtion_object.amelioritive_action = this.login_amelioration
-                    }
-                }
-                break;
-            }
-            case 'logout' : {
-                return super.process_user(user_op,post_body,req,res,primary_key)
-            }
-            case 'register' : {
-                if ( this.db ) {
-                    let user = await this.db.fetch_user(post_body)  // is this service dealing with this user?...
-                    if ( !(user) ) {
-                        if ( post_body.cid ) {
-                            let user_ipfs = await this.db.fetch_user_ipfs(post_body)
-                            await this.db.store_user(user_ipfs)         /// CREATE USER ENTRY IN DB
-                            if ( this.business ) this.business.process('new-user',post_body)
-                            transtion_object.secondary_action = false
-                            transtion_object.token = 'user+' + uuid()   // transaction token
-                            // sha255 hash - unless application override of hashable field from config. (example uses email and password)
-                            let registration_token = this.do_hash(post_body[this.hashables.field1] + post_body[this.hashables.field2])
-                            transtion_object.session_token = 'sess+' + registration_token
-                            transtion_object.elements = { "match" : 'sess+' + registration_token }  // will look for this...
-                            break;
-                        }
-                    }
-                }
-                transtion_object.amelioritive_action = this.registration_fail_amelioration
-                break;
-            }
-            default : {
-                break;
-            }
-        }
-
-        return(transtion_object)
+            return aes_key
+        } catch(e){}
+        //
+        return false
     }
 
 
+    async aes_encryptor(encodable,aes_key,nonce) {
+
+        let enc = new TextEncoder();
+        let clear_buf =  enc.encode(encodable);
+        let iv = nonce
+    
+        let ciphertext = await g_crypto.encrypt({
+                                                    name: "AES-CBC",
+                                                    iv
+                                                },
+                                                aes_key,
+                                                clear_buf
+                                            );
+        return ciphertext
+    }
+
+
+    async key_wrapper(key_to_wrap,pub_wrapper_key) {
+        try {
+            let wrapper_jwk = JSON.parse(pub_wrapper_key)
+            let wrapper = await g_crypto.importKey(
+                    "jwk",
+                    wrapper_jwk,
+                    {   //these are the wrapping key's algorithm options
+                        name: "RSA-OAEP",
+                        modulusLength: 4096, //can be 1024, 2048, or 4096
+                        publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
+                        hash: { name: "SHA-256" }, //can be "SHA-1", "SHA-256", "SHA-384", or "SHA-512"
+                    },
+                    true,
+                    ["wrapKey"]
+            );
+    
+            let wrapped_key = await g_crypto.wrapKey(
+                                                "jwk",
+                                                key_to_wrap,
+                                                wrapper,
+                                                {   //these are the wrapping key's algorithm options
+                                                    name: "RSA-OAEP"
+                                                }
+                                            );
+            let type8 = new Uint8Array(wrapped_key)
+            let tranportable = hex_fromTypedArray(type8)
+            return tranportable
+        } catch(e) {
+            console.log(e)
+        }
+        return false
+    }
+
+    //
+    async gen_wrapped_key(wrapper_public_key) {  // generate a wrapped aes key...
+        let aes_key = await this.gen_cipher_key()
+        let wrapped_key = await key_wrapper(aes_key,wrapper_public_key)
+        return wrapped_key  // let descendants implement
+    }
+
+    //
+    async cipher(clear_text,aes_key,nonce) {
+        let enecrypted =  await this.aes_encryptor(clear_text,aes_key,nonce)
+        return enecrypted               // let descendants implement
+    }
+
+    //
+    async verifier(was_signed_data,signature,signer_pub_key) {
+        try {
+            let signer_jwk = JSON.parse(signer_pub_key)
+            let verifier = await g_crypto.importKey(
+                    "jwk",
+                    signer_jwk,
+                    {
+                        'name': "ECDSA",
+                        'namedCurve': "P-384"
+                    },
+                    true,
+                    ["verify"]
+            );
+            //
+            let enc = new TextEncoder();
+            let verifiable = enc.encode(was_signed_data);
+    
+            let sig_bytes = hex_toByteArray(signature)
+    
+            let result = await g_crypto.verify({
+                                                name: "ECDSA",
+                                                hash: {name: "SHA-384"},
+                                            },
+                                            verifier,
+                                            sig_bytes,
+                                            verifiable
+                                        );
+            return result
+        }  catch(e) {
+            console.log(e)
+        }
+    }
+
+
+    foreign_auth(post_body) {  // check that it is in supported strategies
+        return( false )
+    }
+
+    // // 
+    async login_transition(user,transtion_object,post_body) {
+        if ( user.cid === post_body.cid ) {
+            //
+            transtion_object.secondary_action = true
+            let key_key = this.key_for_user()
+            transtion_object._t_u_key = key_key
+            transtion_object.user_key = user[key_key]
+            this.loginTransitionFields(transtion_object,post_body,user)
+            let [wkey,aes_key] = await this.gen_wrapped_key(user.public_key)
+            transtion_object.wrapped_key = wkey
+            let challenge = gen_nonce()
+            let iv_nonce = gen_nonce()
+            transtion_object.ctext = await this.cipher(challenge,aes_key,iv_nonce)
+            transtion_object.iv_nonce = iv_nonce
+            transtion_object.elements["clear"] = challenge
+            transtion_object.elements["verify_key"] = user.signer_public_key
+            //
+        }
+    }
+
+    // // 
+    async registration_transition(post_body,transtion_object) {
+        if ( post_body.cid ) {
+            let user_ipfs = await this.db.fetch_user_ipfs(post_body)
+            await this.db.store_user(user_ipfs)         /// CREATE USER ENTRY IN DB
+            if ( this.business ) this.business.process('new-user',post_body)
+            transtion_object.secondary_action = false
+            transtion_object.token = 'user+' + uuid()   // transaction token
+            // sha255 hash - unless application override of hashable field from config. (example uses email and password)
+            let registration_token = this.do_hash(post_body[this.hashables.field1] + post_body[this.hashables.field2])
+            transtion_object.session_token = 'sess+' + registration_token
+            transtion_object.elements = { "match" : 'sess+' + registration_token }  // will look for this...
+            return true;
+        }
+        return false
+    }
 
     // //
     async process_user(user_op,body,req,res) {
@@ -150,6 +236,7 @@ class CaptchaSessionManager extends SessionManager {
         return(transtionObj)
     }
 
+    //
     //  process_asset(asset_id,post_body) {}
     feasible(transition,post_body,req) {                // is the transition something that can be done?
         if (  G_captcha_trns.tagged(transition) || G_contact_trns.tagged(transition) ) {
@@ -188,11 +275,10 @@ class CaptchaSessionManager extends SessionManager {
             post_body._t_match_field = post_body[G_users_trns.match_key()]
         } else if ( G_users_trns.secondary_action_selector(transtion_object.action) ) {
             if ( user_op === 'login' ) {    // ----
-                // 
                 let signature = post_body.signature
                 let clear_data = transtion_object.elements["clear"]
                 let verify_key = transtion_object.elements["verify_key"]
-                let OK = await this.verify(signature,clear_data,verify_key)
+                let OK = await this.verifier(clear_data,signature,verify_key)
                 return OK
             } else {
                 post_body._t_match_field = post_body[G_users_trns.secondary_match_key()]
