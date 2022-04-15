1f01008af388e2f41323c238f07ca569f19cce75
Richard Albert Leddy <richardaleddy@gmail.com>
2022-04-15 14:25:40 -0700

    expose libraries use as super classes in implementations

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
+// ----
+
 const CopiousTransitions = require('./user_service_class')
 
-
 module.exports = CopiousTransitions
+
+// AUTH ----
+
+let { GeneralAuth, SessionManager } = require('./lib/general_auth')
+module.exports.GeneralAuth = GeneralAuth
+module.exports.SessionManager = SessionManager
+// will expose {GeneralAuth,SessionManager,SessionManager_Lite}
+module.exports.auth_session_lite = require('./lib/general_auth_session_lite')
+//
+module.exports.GeneralMiddleWare = require('./lib/general_mware')
+module.exports._set_conf_and_return = require('./lib/general_express')
+
+// Transtitions - custom operations and custom global variables
+module.exports.GeneralTransitionEngine = require('./lib/general_transition_engine')
+module.exports.TaggedTransition = require('./lib/tagged_transitions')
+
+// ---- ----  ----  ----  ---- 
+module.exports.DBClass = require('./lib/general_db')
+module.exports.GeneralDynamic = require('./lib/general_dynamic')
+module.exports.GeneralStatic = require('./lib/general_static')
+module.exports.GeneralBusiness = require('./lib/general_business')
+
+// ---- ----  ----  ----  ---- 
+module.exports.GeneralValidator = require('./lib/general_validator')
+module.exports.ReMailer = require('./lib/remailer')
+module.exports.ShutdownManager = require('./lib/shutdown-manager')
+
+// ---- ----  ----  ----  ---- 
+module.exports.CustomPersistenceDB = require('./custom_storage/persistent_db')
+module.exports.CustomStaticDB = require('./custom_storage/static_db')
