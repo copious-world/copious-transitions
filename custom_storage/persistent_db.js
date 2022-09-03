090aef95abc4b73c9efe47b891400c56915915c1
Richard Albert Leddy <richardaleddy@gmail.com>
2022-09-02 20:41:07 -0700

    The persistenceDB default/custom db
    Put in the right number of parameters carried in the call from  general DB.

--- custom_storage/persistent_db.js
+++ custom_storage/persistent_db.js
@@ -4,8 +4,8 @@ let FilesAndRelays = require('./files_and_relays')
 
 class PesistenceContracts extends FilesAndRelays {
 
-    constructor(messenger) {        // eg. message_relay_client
-        super(messenger)
+    constructor(messenger,stash_interval,default_m_path) {        // eg. message_relay_client
+        super(messenger,stash_interval,default_m_path)
     }
 
     initialize(conf) {
